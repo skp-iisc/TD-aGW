fc0262a70d1fee70506c0dbc5f371e45e63bb0ef
skp-iisc <sumanpal@iisc.ac.in>
2026-07-19 05:07:54 +0530

    modified figures

--- gw-bse-silicon/13-plots/absorption.py
+++ gw-bse-silicon/13-plots/absorption.py
@@ -2,50 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Read the data (lines starting with '#' are ignored)
-data = np.loadtxt("../10-absorption/absorption_eh.dat", comments="#")
-# data = np.loadtxt("../10-absorption/absorption_noeh.dat", comments="#")
+data1 = np.loadtxt("../10-absorption/absorption_eh.dat", comments="#")
+data2 = np.loadtxt("../10-absorption/absorption_noeh.dat", comments="#")
 
 # Extract columns
-omega = data[:, 0]
-eps2  = data[:, 1]
-eps1  = data[:, 2]
-jdos  = data[:, 3]
+omega_1 = data1[:, 0]
+eps2_1  = data1[:, 1]
+eps1_1  = data1[:, 2]
+jdos_1  = data1[:, 3]
+
+omega_2 = data2[:, 0]
+eps2_2  = data2[:, 1]
+eps1_2  = data2[:, 2]
+jdos_2  = data2[:, 3]
 
 # Plot: Imaginary Dielectric Function (epsilon2) vs omega
 
 plt.figure(figsize=(6, 4))
 # plt.axhline(0.0, color='k', linewidth=1)
 # plt.axvline(0.0, color='k', linewidth=1)
-plt.plot(omega, eps2, linewidth=1.5)
+plt.plot(omega_1, eps2_1, linewidth=1.5, label='with e-h interaction')
+plt.plot(omega_2, eps2_2, linewidth=1.5, label='without e-h interaction')
 plt.xlabel(r'$\omega$', fontsize=14)
 plt.ylabel(r'$\epsilon_2(\omega)$', fontsize=14)
 plt.tick_params(direction='in', top=True, right=True)
+plt.legend(fontsize=12)
 plt.tight_layout()
-plt.savefig("si_eps2_eh.png", dpi=300)
-# plt.savefig("si_eps2_noeh.png", dpi=300)
+plt.savefig("si_gw_bse_eps2.png", dpi=300)
 
 # Plot: Real Dielectric Function (epsilon1) vs omega
 
 plt.figure(figsize=(6, 4))
 # plt.axhline(0.0, color='k', linewidth=1)
 # plt.axvline(0.0, color='k', linewidth=1)
-plt.plot(omega, eps1, linewidth=1.5)
+plt.plot(omega_1, eps1_1, linewidth=1.5, label='with e-h interaction')
+plt.plot(omega_2, eps1_2, linewidth=1.5, label='without e-h interaction')
 plt.xlabel(r'$\omega$', fontsize=14)
 plt.ylabel(r'$\epsilon_1(\omega)$', fontsize=14)
 plt.tick_params(direction='in', top=True, right=True)
+plt.legend(fontsize=12)
 plt.tight_layout()
-plt.savefig("si_eps1_eh.png", dpi=300)
-# plt.savefig("si_eps1_noeh.png", dpi=300)
+plt.savefig("si_gw_bse_eps1.png", dpi=300)
 
 # Plot: Joint Density of States (JDOS) vs omega
 
 plt.figure(figsize=(6, 4))
 # plt.axhline(0.0, color='k', linewidth=1)
 # plt.axvline(0.0, color='k', linewidth=1)
-plt.plot(omega, jdos, linewidth=1.5)
+plt.plot(omega_1, jdos_1, linewidth=1.5, label='with e-h interaction')
+plt.plot(omega_2, jdos_2, linewidth=1.5, label='without e-h interaction')
 plt.xlabel(r'$\omega$', fontsize=14)
 plt.ylabel('JDOS', fontsize=14)
 plt.tick_params(direction='in', top=True, right=True)
+plt.legend(fontsize=12)
 plt.tight_layout()
-plt.savefig("si_jdos_eh.png", dpi=300)
-# plt.savefig("si_jdos_noeh.png", dpi=300)
+plt.savefig("si_gw_bse_jdos.png", dpi=300)
